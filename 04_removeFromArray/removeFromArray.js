36bc91dab32e4788f8a09ad6064d4e3744f69757
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-14 16:33:07 +0100

    Complete JS exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < removeItems.length; j++) {
+            if (removeItems[j] === array[i]) { 
+                array.splice(i, 1);
+                j -= 2; // reset the counter to account for reduced array
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
