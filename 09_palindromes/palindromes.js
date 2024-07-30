f234cb2d0f45ee8dbd46503326c140bbeb6fb5d8
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-30 21:37:45 +0100

    Complete JS exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    let singleString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let length = singleString.length;
 
+    for (let i = 0; i < length; i++) {
+        if (singleString[i].toLowerCase() !== singleString[length - 1 - i].toLowerCase()) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
