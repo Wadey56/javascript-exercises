c97673bb97272f1c7fee35a3535f4a48faf4df09
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-31 21:46:57 +0100

    Complete JS exercide 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function calcAge(yearOfDeath, yearOfBirth) {
+    if (!yearOfDeath) {
+        return (new Date().getFullYear()) - yearOfBirth;
+    } else {
+        return yearOfDeath - yearOfBirth;
+    }
+}
 
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+
+        let currentAge = calcAge(current["yearOfDeath"], current["yearOfBirth"]);
+        let oldestAge = calcAge(oldest["yearOfDeath"], oldest["yearOfBirth"]);
+
+        if (currentAge > oldestAge) {
+            return oldest = current;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
