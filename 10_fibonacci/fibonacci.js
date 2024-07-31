f49ed2c3dcecb427292773006357871793f9d492
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-31 20:12:23 +0100

    Complete JS exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    number = Number(number);
+
+    if (number < 0) {
+        return "OOPS";
+    } else if (number == 0) {
+        return 0;
+    }
+    
+    let fib = [1, 1];
+    for (let i = 2; i < number; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[number - 1];
 };
 
 // Do not edit below this line
