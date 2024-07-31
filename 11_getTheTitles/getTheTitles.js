d2805f674218f7efcb37134c74c28ca95fb624b5
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-31 20:25:54 +0100

    Complete JS exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let titles = array.map((book) => {
+        return book["title"];
+    })
 
+    return titles;
 };
 
 // Do not edit below this line
